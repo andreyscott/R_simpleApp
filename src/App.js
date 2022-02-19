2b9fb3d57e1309744ae07a1f715f8151f9afdfb1
andreyscott <andreyscott>
2022-02-19 20:25:12 +0100

    work it out

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+//import React from 'react';
+// import counter from './component/counter';
+import { Counter } from './component/counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p className="App-intro">
+        To Simple Counter code work that increment by 3 and decrement by 2  because why not 😉
+      </p>
+      <Counter />
       </header>
     </div>
   );
